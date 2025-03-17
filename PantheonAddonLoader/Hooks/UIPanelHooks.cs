a95d241332a77ecad3b00943f09e472b8d5e7361
owisx <jasonmwegner@gmail.com>
2025-03-16 21:30:10 -0500

    UIPanel hooks

--- PantheonAddonLoader/Hooks/UIPanelHooks.cs
+++ PantheonAddonLoader/Hooks/UIPanelHooks.cs
@@ -3,6 +3,7 @@ using Il2Cpp;
 using PantheonAddonFramework;
 using PantheonAddonLoader.AddonManagement;
 using PantheonAddonLoader.UI;
+using PantheonAddonFramework.Models;
 
 namespace PantheonAddonLoader.Hooks;
 
@@ -15,5 +16,27 @@ public class UIPanelHooks
         {
             AddonLoader.WindowPanelEvents.OnExperienceBarReady.Raise(new XpBarWindow(__instance));
         }
+        if (__instance.name == "Panel_OffensiveTarget")
+        {
+            var healthBarPool = __instance.GetComponentInChildren<UIPoolBar>(true);
+            AddonLoader.WindowPanelEvents.OnOffensiveTargetReady.Raise(new AddonPoolBar(healthBarPool));
+        }
+    }
+}
+
+[HarmonyPatch(typeof(UIPoolBar), nameof(UIPoolBar.HandlePoolChanged))]
+public class UIPoolBarHandlePoolChangedHook
+{
+    private static void Postfix(UIPoolBar __instance, float current, float max, float delta, PoolChangeType changeType)
+    {
+        var pppName = __instance.transform.parent.parent.parent.name;
+
+        if (pppName == "Panel_OffensiveTarget")
+        {
+            var percent = current / max * 100;
+            AddonLoader.WindowPanelEvents.OnOffTargetPoolbarChange.Raise(percent);
+        }
+
+
     }
 }
